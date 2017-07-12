5c8f89c0693f046eedb7435d3bef3fe191a40ea3
Telnov Oleg <telnov1971@gmail.com>
2017-07-12 10:17:57 +0600

    Progress indicator is added

--- fbrename.py
+++ fbrename.py
@@ -1,27 +1,25 @@
 """ This module renames files from the name of the book """
-import pathlib
-import zipfile
+from pathlib import Path
+from zipfile import is_zipfile, ZipFile
 import sys
 
 
 # coding: utf8
-__version__ = '0.4'
+__version__ = '0.45'
 __author__ = 'Telnov Oleg'
-
-
-__path__ = pathlib.Path('.')
-__list_of_files_fb__ = list(__path__.glob('*.fb2'))
-__list_of_files_zip__ = list(__path__.glob('*.zip'))
-__list_of_files__ = __list_of_files_fb__ + __list_of_files_zip__
 
 try:
     __log__ = open('error.log', 'w', encoding='utf-8')
 except OSError:
     print('I can not open the file "error.log"')
 
-if __list_of_files__ is None:
-    __log__.write('Books not found')
-    quit()
+
+def get_list_fb(default_path='.'):
+    ''' get_list_fb() - List the files FB2 in the directory '''
+    the_path = Path(default_path)
+    list_of_files_fb = list(the_path.glob('*.fb2'))
+    list_of_files_zip = list(the_path.glob('*.zip'))
+    return list_of_files_fb + list_of_files_zip
 
 
 def findstr(_text, _str):
@@ -69,40 +67,49 @@ def decoder(_binary):
         return _content
 
 
-__I__ = 0
-__P__ = 100 / len(__list_of_files__)
-
-for f in __list_of_files__:
-    __I__ += 1
-    per = '\t' + str(round(__I__*__P__, 2))
-    print(per + '%', end='\r')
-    try:
-        fname = ''
-        arr_bin = []
-        if zipfile.is_zipfile(f):
-            zf = zipfile.ZipFile(str(f), mode='r')
-            zl = zf.namelist()
-            if len(zl) == 1:
-                if zl[0][-3:] == 'fb2':
-                    fb = zf.open(zl[0], mode='r')
-                    arr_bin = fb.read()
-                    fb.close()
-                    zf.close()
+def __main__():
+    ''' __main__ - main circle program '''
+    list_of_files = get_list_fb()
+    if list_of_files is None:
+        __log__.write('Books not found')
+        quit()
+    percent = 100 / len(list_of_files)
+    ind = 0
+    for flb in list_of_files:
+        ind += 1
+        per = round(ind*percent, 2)
+        progress = '|' + ('*'*round(per // 5)) + (' '*(20 - round(per // 5))) + '|'
+        print('%s %6.2f' % (progress,per), end='\r')
+        try:
+            fname = ''
+            if is_zipfile(flb):
+                zpfl = ZipFile(str(flb), mode='r')
+                zplst = zpfl.namelist()
+                if len(zplst) == 1:
+                    if zplst[0][-3:] == 'fb2':
+                        fb2 = zpfl.open(zplst[0], mode='r')
+                        arr_bin = fb2.read()
+                        fb2.close()
+                        zpfl.close()
+                else:
+                    zpfl.close()
+                    continue
             else:
-                zf.close()
-                continue
-        else:
-            fb = open(f, 'rb')
-            arr_bin = fb.read()
-            fb.close()
-        if arr_bin is not None:
-            text = decoder(arr_bin)
-            fname = getname(text, f)
-            if not fname == '':
-                if not pathlib.Path(fname).exists():
-                    f.rename(fname)
-    except OSError:
-        zf.close()
-        fb.close()
-        __log__.write(str(f) + '\t->\t' + fname + '\n')
-        __log__.write(str(sys.exc_info()) + '\n')
+                fb2 = open(flb, 'rb')
+                arr_bin = fb2.read()
+                fb2.close()
+            if arr_bin is not None:
+                text = decoder(arr_bin)
+                fname = getname(text, flb)
+                if not fname == '':
+                    if not Path(fname).exists():
+                        flb.rename(fname)
+        except OSError:
+            zpfl.close()
+            fb2.close()
+            __log__.write(str(flb) + '\t->\t' + fname + '\n')
+            __log__.write(str(sys.exc_info()) + '\n')
+    print()
+    input("Press Enter")
+
+__main__()
